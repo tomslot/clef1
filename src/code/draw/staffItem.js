0a30154feca21207498b16baf1aa33f5162e695a
Tomasz Slota <tomslot@gmail.com>
2018-09-06 22:45:32 +0200

    refined chord rendering

--- src/code/draw/staffItem.js
+++ src/code/draw/staffItem.js
@@ -4,69 +4,90 @@ import {staffMetrics} from './staffMetrics.js';
 
 const HIT_CHORD_NOTE_COLOR = "#70A070";
 const SHADOW_COLOR = style.COLOR3;
+const NOTE_TAIL_SIZE = 55;
 
 export const staffItem = {
-    draw(ctx, staffItem) {
-        for (const note of staffItem.notes){
-            this.drawNote(ctx, note);
-        }
-    },
-
-    drawNote(ctx, note){
+    draw(ctx, item) {
         const staffWidth = staffMetrics.rightScoreEnd - staffMetrics.leftScoreStart;
         const noteX = staffMetrics.rightScoreEnd - game.noteProgress * staffWidth;
-        const noteY = staffMetrics.calcNoteY(note);
+        ctx.save();
+            ctx.shadowBlur = 5;
+            ctx.shadowOffsetX = 3;
+            ctx.shadowOffsetY = 3;
+            ctx.shadowColor = SHADOW_COLOR;
 
-        let noteColor = HIT_CHORD_NOTE_COLOR;
-
-        if (!note.hit){
             const redAmount = parseInt(Math.pow(game.noteProgress, 2) * 255);
-            noteColor = `rgb(${redAmount}, 0, 0)`;
-        }
+            const noteColor = `rgb(${redAmount}, 0, 0)`;
+            ctx.fillStyle = noteColor;
+            ctx.strokeStyle = noteColor;
 
-        ctx.save();
+            if (game.hitAnimation > 0){
+                const shift = (1 - game.hitAnimation) * 150;
+                ctx.translate(shift, -1 * shift);
+                ctx.translate(noteX, staffMetrics.midGPos);
+                ctx.rotate((1 - game.hitAnimation) * Math.PI);
+                ctx.translate(-1 * noteX, -1 * staffMetrics.midGPos);
+            }
 
-        if (game.hitAnimation > 0){
-            const shift = (1 - game.hitAnimation) * 150;
-            ctx.translate(shift, -1 * shift);
-            ctx.translate(noteX, noteY);
-            ctx.rotate((1 - game.hitAnimation) * Math.PI);
-            ctx.translate(-1 * noteX, -1 * noteY);
-        }
+            this.drawTail(ctx, item, noteX);
 
-        ctx.shadowBlur = 5;
-        ctx.shadowOffsetX = 3;
-        ctx.shadowOffsetY = 3;
-        ctx.shadowColor = SHADOW_COLOR;
-        ctx.fillStyle = noteColor;
-        ctx.strokeStyle = noteColor;
-        ctx.save();
-        let yScale = 0.75;
-        ctx.scale(1, yScale);
-        ctx.beginPath();
-        ctx.arc(noteX, noteY / yScale, 9, 0, Math.PI * 4, false);
-        ctx.fill();
-        ctx.stroke();
-        ctx.closePath();
-        ctx.restore();
-        ctx.save();
-        ctx.lineWidth = 3;
-        ctx.beginPath();
-        ctx.moveTo(noteX + 8, noteY);
-        let dir = note.distanceFromMidG > 4 ? -1 : 1;
-        ctx.lineTo(noteX + 7, noteY - 55 * dir);
-        ctx.closePath();
-        ctx.stroke();
-        ctx.restore();
-        ctx.beginPath();
-
-        if (note.distanceFromMidG === -4 || note.distanceFromMidG === 8) {
-            ctx.moveTo(noteX - 20, noteY);
-            ctx.lineTo(noteX + 20, noteY);
-        }
-
-        ctx.stroke();
-        ctx.closePath();
+            for (const note of item.notes){
+                this.drawNote(ctx, note, noteX);
+            }
         ctx.restore();
     },
+
+    drawNote(ctx, note, noteX){
+        const noteY = staffMetrics.calcNoteY(note);
+
+        ctx.save();
+            if (note.hit){
+                ctx.fillStyle = HIT_CHORD_NOTE_COLOR;
+            }
+
+            if (note.distanceFromMidG === -4 || note.distanceFromMidG === 8) {
+                ctx.beginPath();
+                ctx.moveTo(noteX - 20, noteY);
+                ctx.lineTo(noteX + 20, noteY);
+                ctx.stroke();
+                ctx.closePath();
+            }
+
+            ctx.save();
+                let yScale = 0.75;
+                ctx.scale(1, yScale);
+                ctx.beginPath();
+                ctx.arc(noteX, noteY / yScale, 9, 0, Math.PI * 4, false);
+                ctx.fill();
+                ctx.stroke();
+                ctx.closePath();
+            ctx.restore();
+        ctx.restore();
+    },
+
+    drawTail(ctx, item, noteX){
+        const bottomNote = item.notes[0];
+        const topNote = item.notes[item.notes.length - 1];
+        const dir = topNote.distanceFromMidG > 4 ? -1 : 1;
+
+        let bottomY;
+        let topY;
+
+        if (dir > 0){
+            bottomY = staffMetrics.calcNoteY(bottomNote);
+            topY = staffMetrics.calcNoteY(topNote) - NOTE_TAIL_SIZE;
+        } else {
+            bottomY = staffMetrics.calcNoteY(topNote);
+            topY = staffMetrics.calcNoteY(bottomNote) + NOTE_TAIL_SIZE;
+        }
+
+        ctx.save();
+            ctx.lineWidth = 3;
+            ctx.beginPath();
+            ctx.moveTo(noteX + 8, bottomY);
+            ctx.lineTo(noteX + 7, topY);
+            ctx.closePath();
+            ctx.stroke();
+        ctx.restore();
+    }
 }
--- src/code/draw/staffMetrics.js
+++ src/code/draw/staffMetrics.js
@@ -8,6 +8,7 @@ export const staffMetrics = {
     topLineY: 0,
     leftScoreStart: 0,
     rightScoreEnd: 0,
+    midGPos: 0,
 
     resize() {
         let emptySpace = 3 * this.margin + 2 * this.lineNumber;
@@ -16,10 +17,10 @@ export const staffMetrics = {
         this.topLineY = this.margin + this.lineDistance / 2;
         this.leftScoreStart = this.clefSpace;
         this.rightScoreEnd = this.width - this.margin;
+        this.midGPos = this.topLineY + 3 * this.lineDistance;
     },
 
     calcNoteY(note){
-        const midGPos = this.topLineY + 3 * this.lineDistance;
-        return midGPos - note.distanceFromMidG * this.lineDistance / 2;
+        return this.midGPos - note.distanceFromMidG * this.lineDistance / 2;
     },
 }
